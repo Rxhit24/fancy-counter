02df73521bde4cdbedc414b5aabab1d3919e7669
Rxhit24 <prasadrohit24@gmail.com>
2025-02-11 16:39:11 +0530

    V2

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ import './App.css'
 function App() {
   return (
     <>
-      <div style={{height:'100vh' , backgroundColor:'aqua'}}>Hello World</div>
+      <div style={{height:'100vh' , backgroundColor:'aqua'}}>
+        <h1>Hello World</h1>
+        <h2>Test</h2>
+      </div>
     </>
   )
 }
